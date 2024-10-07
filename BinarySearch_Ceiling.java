3e5e0b42eaa969f62f87e838f68dda2b8b41f9ad
Aayushbhatt06 <aayushbhatt2006@gmail.com>
2024-10-08 00:00:37 +0530

    updated the ceiling and floor number code to return -1 when thedesired number is impossible to be found

--- BinarySearch_Ceiling.java
+++ BinarySearch_Ceiling.java
@@ -1,7 +1,7 @@
 public class BinarySearch_Ceiling {
     public static void main(String[] args) {
         int[] arr = {2,15,22,25,43,56,89,100,152,186};
-        int ans = ceiling(arr, 125);
+        int ans = ceiling(arr, 1250);
         System.out.println(ans);
     }
 
@@ -10,6 +10,9 @@ public class BinarySearch_Ceiling {
         int end = arr.length-1;
         while(start <= end){
             int mid = start + (end - start)/2;
+            if(target > arr[arr.length-1]){
+                return -1;
+            }
             if(target < arr[mid]){
                 end = mid - 1;
 
--- BinarySearch_Floor.java
+++ BinarySearch_Floor.java
@@ -1,7 +1,7 @@
 public class BinarySearch_Floor {
     public static void main(String[] args) {
         int[] arr = {2,15,22,25,43,56,89,100,152,186};
-        int ans = floor(arr, 125);
+        int ans = floor(arr, 1);
         System.out.println(ans);
     }
 
@@ -10,6 +10,9 @@ public class BinarySearch_Floor {
         int end = arr.length-1;
         while(start <= end){
             int mid = start + (end - start)/2;
+            if(target < arr[0]){
+                return -1;
+            }
             if(target < arr[mid]){
                 end = mid - 1;
 
