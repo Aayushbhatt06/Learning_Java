157a96f8cc501df32c51496efe9c8ad38b9fc2b0
Aayush Bhatt <152008288+Aayushbhatt06@users.noreply.github.com>
2025-08-03 18:12:42 +0530

    maximum subarray

--- LC_53.java
+++ LC_53.java
@@ -1,19 +1,39 @@
-
 class LC_53 {
 
     public static int maxSubArray(int[] nums) {
-        int max_so_far = nums[0];
-        int max_curr = nums[0];
-        for(int i = 1 ; i < nums.length ; i++){
-            max_curr = Math.max(nums[i],nums[i]+max_curr);
-            max_so_far = Math.max(max_so_far,max_curr);
+        int max_so_far = Integer.MIN_VALUE;
+        int sum = 0;
+        int start = 0;
+        int end = 0;
+        int tempStart = 0;
 
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if(sum > max_so_far) {
+                max_so_far = sum;
+                start = tempStart;
+                end = i;
+            }
+
+            if(sum < 0) {
+                sum = 0;
+                tempStart = i + 1;
+            }
         }
+
+        // Print the subarray
+        System.out.print("Subarray with max sum: ");
+        for(int i = start; i <= end; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println();
+
         return max_so_far;
     }
 
     public static void main(String[] args) {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println(maxSubArray(nums));
+        System.out.println("Max Subarray Sum: " + maxSubArray(nums));
     }
 }
