b62ba1dd9dfb1d74c5a29783159b06e6086f228c
Aayush Bhatt <152008288+Aayushbhatt06@users.noreply.github.com>
2026-05-10 17:14:08 +0530

    printing LCS

--- LeetCode/LC_1143.java
+++ LeetCode/LC_1143.java
@@ -49,4 +49,47 @@ class LC_1143 {
 
         return dp[0][0];
     }
+
+    public String printLCS(String text1, String text2) {
+
+        int n = text1.length();
+        int m = text2.length();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        // Build DP table
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+
+        // Reconstruct answer
+        int i = 0;
+        int j = 0;
+
+        StringBuilder ans = new StringBuilder();
+
+        while (i < n && j < m) {
+
+            // Match
+            if (text1.charAt(i) == text2.charAt(j)) {
+                ans.append(text1.charAt(i));
+                i++;
+                j++;
+            } // Move in direction of larger value
+            else if (dp[i + 1][j] > dp[i][j + 1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return ans.toString();
+    }
 }
